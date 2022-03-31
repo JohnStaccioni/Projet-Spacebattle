d9d6ff8eab08dbe42e35bdf53485908ef3bd85ba
John <john.staccioni@hotmail.fr>
2022-03-31 15:32:22 +0200

    Niveau 0 sans commentaire

---
+++ niveau0/html/search/functions_3.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['init_46',['init',['../main_8c.html#a9c0e87d6a96e4bb78e7ff9509498c713',1,'main.c']]],
+  ['init_5fdata_47',['init_data',['../main_8c.html#a5676e72ded3c1a993c4c975ef9958288',1,'main.c']]],
+  ['init_5fsdl_48',['init_sdl',['../sdl2-light_8c.html#a7307299b4ef47feaa1a4e95341a04c7a',1,'sdl2-light.c']]],
+  ['init_5ftextures_49',['init_textures',['../main_8c.html#a03798323fe88fdd815fa2cb29b79f972',1,'main.c']]],
+  ['is_5fgame_5fover_50',['is_game_over',['../main_8c.html#a87c51ee2e71f0707b7c964afc414f556',1,'main.c']]]
+];
