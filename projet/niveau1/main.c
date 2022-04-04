a1fa9f531b0986e19c0bde90ee572508a6d32f3f
John <john.staccioni@hotmail.fr>
2022-04-04 16:30:52 +0200

    Niveau 1 avec vaisseau

--- projet/niveau1/main.c
+++ projet/niveau1/main.c
@@ -41,7 +41,7 @@
 
 struct textures_s{
     SDL_Texture* background; /*!< Texture liée à l'image du fond de l'écran. */
-    /* A COMPLETER */
+    SDL_Texture* main_ship; /*!< Texture liée au vaisseau du joueur. */
 };
 
 
@@ -51,18 +51,31 @@ struct textures_s{
 
 typedef struct textures_s textures_t;
 
+/**
+ * \brief Structure pour gérer le placement du vaisseau
+ */
+
+struct sprite_s{
+    SDL_Texture * player_ship; /*!< Texture liée au vaisseau */
+    int x; /*!< abscisse du centre du vaisseau */
+    int y; /*!< ordonné du centre du vaisseau */
+    int w; /*!<largeur du sprite */
+    int h; /*!<hauteur du sprite */
+    int v; /*!< vitesse verticale du sprite */
+};
+
+/**
+ * \brief Type qui correspond à sprite_s
+ */
+typedef struct sprite_s sprite_t;
 
 /**
  * \brief Représentation du monde du jeu
 */
 
 struct world_s{
-    /*
-      A COMPLETER
-     */
-    
     int gameover; /*!< Champ indiquant si l'on est à la fin du jeu */
-
+    sprite_t main_ship; /*!< Vaisseau du joueur */
 };
 
 /**
@@ -71,30 +84,50 @@ struct world_s{
 
 typedef struct world_s world_t;
 
+/**
+ * \brief programme de test de l'affichage du Vaisseau du joueur
+ * \param sprite pointeur vers la structure sprite_t du vaisseau du joueur
+ */
+ void print_sprite(sprite_t * sprite){
+    printf("Abscisse du centre du sprite %d \nOrdonné du centre du sprite %d\nLargeur du sprite %d\nHauteur du sprite %d\nVitesse verticale du sprite %d\n", sprite->x, sprite->y, sprite->h, sprite->w, sprite->v);
+ }
 
+/**
+ * \brief fonction qui initalise le sprite
+ * \param sprite texture du vaisseau du joueur
+ * \param x abscisse du centre du sprite
+ * \param y ordonné du centre du sprite
+ * \param w largeur du sprite
+ * \param h hauteur du sprite
+ * \param v vitesse verticale du sprite
+*/
 
-
+void init_sprite(sprite_t *sprite, int x, int y, int w, int h, int v){
+    sprite->x = x;
+    sprite->y = y;
+    sprite->w = w;
+    sprite->h = h;
+    sprite->v = v;
+}
 
 /**
  * \brief La fonction initialise les données du monde du jeu
  * \param world les données du monde
  */
-
-
 void init_data(world_t * world){
     
     //on n'est pas à la fin du jeu
     world->gameover = 0;
-    
+    init_sprite(&(world->main_ship), (SCREEN_WIDTH/2-SHIP_SIZE/2),SCREEN_HEIGHT-3*SHIP_SIZE/2,SHIP_SIZE,SHIP_SIZE,10);
+    print_sprite(&(world->main_ship));
 }
+
 
 
 /**
  * \brief La fonction nettoie les données du monde
  * \param world les données du monde
  */
-
-
 void clean_data(world_t *world){
     /* utile uniquement si vous avez fait de l'allocation dynamique (malloc); la fonction ici doit permettre de libérer la mémoire (free) */
     
@@ -143,11 +176,18 @@ void handle_events(SDL_Event *event,world_t *world){
        
          //si une touche est appuyée
          if(event->type == SDL_KEYDOWN){
+             if(event->key.keysym.sym == SDLK_RIGHT){
+                 world->main_ship.x += 8;
+             }
+             if(event->key.keysym.sym == SDLK_LEFT){
+                 world->main_ship.x -= 8;
+             }
              //si la touche appuyée est 'D'
              if(event->key.keysym.sym == SDLK_d){
                  printf("La touche D est appuyée\n");
               }
          }
+        print_sprite(&(world->main_ship));
     }
 }
 
@@ -159,7 +199,7 @@ void handle_events(SDL_Event *event,world_t *world){
 
 void clean_textures(textures_t *textures){
     clean_texture(textures->background);
-    /* A COMPLETER */
+    clean_texture(textures->main_ship);
 }
 
 
@@ -172,10 +212,7 @@ void clean_textures(textures_t *textures){
 
 void  init_textures(SDL_Renderer *renderer, textures_t *textures){
     textures->background = load_image( "ressources/space-background.bmp",renderer);
-    
-    /* A COMPLETER */
-
-    
+    textures->main_ship = load_image("ressources/spaceship.bmp", renderer);
 }
 
 
@@ -192,7 +229,20 @@ void apply_background(SDL_Renderer *renderer, textures_t *textures){
 }
 
 
-
+/**
+ * \brief La fonction qui applique la texture associée au sprite
+ * \param renderer moteur de rendu
+ * \param texture image du sprite
+ * \param sprite structure contenant les informations liés au sprite
+*/
+void apply_sprite(SDL_Renderer *renderer, SDL_Texture *texture, sprite_t*sprite){
+    SDL_Rect dst = {0, 0, 0, 0};
+    
+    SDL_QueryTexture(texture, NULL, NULL, &dst.w, &dst.h);
+    dst.x = sprite->x; dst.y = sprite->y;
+    
+    SDL_RenderCopy(renderer, texture, NULL, &dst);
+}
 
 
 /**
@@ -209,7 +259,7 @@ void refresh_graphics(SDL_Renderer *renderer, world_t *world,textures_t *texture
     
     //application des textures dans le renderer
     apply_background(renderer, textures);
-    /* A COMPLETER */
+    apply_sprite(renderer, textures->main_ship, &(world->main_ship));
     
     // on met à jour l'écran
     update_screen(renderer);
@@ -246,7 +296,6 @@ void init(SDL_Window **window, SDL_Renderer ** renderer, textures_t *textures, w
     init_data(world);
     init_textures(*renderer,textures);
 }
-
 
 /**
  *  \brief programme principal qui implémente la boucle du jeu
